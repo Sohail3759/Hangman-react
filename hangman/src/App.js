dbe1e4e8f1682d26c912acbc3cd15d2f0eaa607d
Sohail3759 <164088898+Sohail3759@users.noreply.github.com>
2024-10-28 18:49:26 +0530

    yoo

--- hangman/src/App.js
+++ hangman/src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 function App(props) {
   return (
-    <div>
-      <h1>helllooooo</h1>
-    </div>
+    <>
+      <div>
+        <h1>helllooooo</h1>
+      </div>
+    </>
+
   );
 }
 
