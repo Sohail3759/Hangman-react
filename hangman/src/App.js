075f35df05af7866c4a26ce7b02aa6eb24ee81dd
Sohail3759 <164088898+Sohail3759@users.noreply.github.com>
2024-11-06 20:20:04 +0530

    deleted unwanted things

--- hangman/src/App.js
+++ hangman/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Hang from './Components/Hang';
 
 function App(props) {
   return (
     <>
       <div>
         <h1>helllooooo</h1>
+        <Hang/>
       </div>
     </>
 
---
+++ hangman/src/Components/Hang.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class Hang extends Component {
+  render() {
+    return (
+      <div>
+        <div className="container">
+            <h1>nikal mc</h1>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Hang
