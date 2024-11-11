141b1f522b3f7cbb8bc547f8e8ddbe38a07035d6
Sohail3759 <164088898+Sohail3759@users.noreply.github.com>
2024-11-11 21:28:23 +0530

    Update Hang.js

--- hangman/src/Components/Hang.js
+++ hangman/src/Components/Hang.js
@@ -3,11 +3,9 @@ import React, { Component } from 'react'
 export class Hang extends Component {
   render() {
     return (
-      <div>
-        <div className="container">
-            <h1>nikal mc</h1>
-        </div>
-      </div>
+      <>
+        
+      </>
     )
   }
 }
